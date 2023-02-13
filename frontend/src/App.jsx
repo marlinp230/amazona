6bbae80f3a2d5143fed53f2886cbf88fb5f36aa7
Marlyn <marlinp230@gmail.com>
2023-02-13 11:50:34 -0500

    addHolaCOMO

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (  
+    <div className="App">
+      <header className="App-header">
+       Hola como esta ?
+      </header>
+      <h1>hello againt</h1>
+    </div> 
+  );
+}
+
+export default App;
